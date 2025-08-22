cc99f4c8769c2ece015259e8888f1d0592ebad74
John Christensen <johnedchristensen@gmail.com>
2025-08-22 12:59:58 -0500

    Update deploy script for bundling homebrew binary

--- foray_ui/deploy_scripts/sign.sh
+++ foray_ui/deploy_scripts/sign.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-cd ../
+cd ../../
 cargo bundle --release
-cd ./deploy_scripts/
+# cd ./deploy_scripts/
 
-codesign -s "University of Wisconsin-Madison" --deep -f -v -o runtime --entitlements ./entitlements.plist ../target/release/bundle/osx/Foray.app
+codesign -s "University of Wisconsin-Madison" --deep -f -v -o runtime --entitlements ./foray_ui/deploy_scripts/entitlements.plist ./target/release/bundle/osx/Foray.app
 
-zip -r ../target/release/bundle/osx/Foray.zip  ../target/release/bundle/osx/Foray.app/
+cd ./target/release/bundle/osx
+rm ./Foray.zip
+zip -r ./Foray.zip  ./Foray.app/
 
 # credentials must already be stored via:
 # xcrun notarytool store-credentials "notarytool-password" \
 #                         --apple-id "johnnyc1423@gmail.com" \
 #                         --team-id C658W77DY8 \
 #                         --password <app specific password created from App-Specific Passwords" https://account.apple.com/account/manage>
-xcrun notarytool submit ../target/release/bundle/osx/Foray.zip --keychain-profile "notarytool-password" --wait
+xcrun notarytool submit ./Foray.zip --keychain-profile "notarytool-password" --wait
 
 # then create a release with something like this:
 # gh release create v0.1.2 --notes "application is now notarized" ./Foray.zip 
