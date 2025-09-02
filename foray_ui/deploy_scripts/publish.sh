de33d2be9d8b90c3160b41e4203a1a0d5a1e54b1
John Christensen <jechristens3@wisc.edu>
2025-09-02 14:55:51 -0500

    Update publish script

--- foray_ui/deploy_scripts/publish.sh
+++ foray_ui/deploy_scripts/publish.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
-cd ../../
-cargo bundle --release
-# cd ./deploy_scripts/
 
+foray_file_zip="./Foray-$1.zip"
+echo "Creating release: $1"
+echo "foray file: $foray_file_zip"
+
+cd ../../
+# Compile
+cargo bundle --release
+
+# Sign
 codesign -s "University of Wisconsin-Madison" --deep -f -v -o runtime --entitlements ./foray_ui/deploy_scripts/entitlements.plist ./target/release/bundle/osx/Foray.app
 
+# Compress
 cd ./target/release/bundle/osx
-rm ./Foray.zip
-zip -r ./Foray.zip  ./Foray.app/
+rm ./Foray*.zip
+zip -r "$foray_file_zip"  ./Foray.app/
 
 # credentials must already be stored via:
 # xcrun notarytool store-credentials "notarytool-password" \
 #                         --apple-id "johnnyc1423@gmail.com" \
 #                         --team-id C658W77DY8 \
 #                         --password <app specific password created from App-Specific Passwords" https://account.apple.com/account/manage>
-xcrun notarytool submit ./Foray.zip --keychain-profile "notarytool-password" --wait
+#
 
-# then create a release with something like this:
-# gh release create v0.1.2 --notes "application is now notarized" ./Foray.zip 
-# todo: change file name to have version/architecture
+# Notarize zip
+xcrun notarytool submit "$foray_file_zip" --keychain-profile "notarytool-password" --wait
 
-# then update uw-mrtud/homebrew-mrtud/ to use the newest version/release
+# Create a release and attach the zip
+gh release create  "$1" --notes "$2" "$foray_file_zip"
+
+# Update brew cask repo
+numeric_version="${1:1}"
+brew bump-cask-pr uw-mrtud/mrtud/foray --version "$numeric_version"
 
